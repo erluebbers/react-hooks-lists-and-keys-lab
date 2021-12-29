89231af1107370f04ec649b1ebf8163d0e94fb1f
Erik Luebbers <erluebbers@gmail.com>
2021-12-29 12:13:36 -0800

    commit tree 2e570c3d0198530cae640436fceb0a3be763c7cb

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const technologyMapped = technologies.map(technology => <span key={technology}>{technology}</span>)
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {technologies.map(technology => <span key={technology}>{technology}</span>)}
+        {technologyMapped}
       </div>
     </div>
   );
